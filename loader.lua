d83480862ae6284e60198fb16bf928f29860a869
komt <64824301+3xq@users.noreply.github.com>
2022-06-28 07:59:46 -0500

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,27 @@
+local script_details = {
+    debug = false,
+    version = "1.0.0",
+}
+
+local url = script_details.debug and "http://localhost:6845" or "https://raw.githubusercontent.com/Averiias/purple-haze-pf/main"
+
+local out = script_details.debug and function(T, ...)
+    return warn("[indigo - debug]: "..T:format(...))
+end or function() end
+
+local function import(file)
+    out("importing file \"%s\"", file)
+    -- return task.spawn(function()
+    local x, a = pcall(function()
+        return loadstring(game:HttpGet(url .. file))()
+    end)
+    if not x then
+        return warn('failed to import', file)
+    end
+    -- end)
+end
+
+getgenv().import = import
+getgenv().details = scriptdetails
+
+import('/main.lua')
