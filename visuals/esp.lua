0f1c7069af198f5ae0abc1c000d52994f967687c
komt <64824301+3xq@users.noreply.github.com>
2022-06-30 04:41:12 -0500

    Add files via upload

---
+++ visuals/esp.lua
@@ -0,0 +1,32 @@
+getgenv().esp = loadstring(game:HttpGet("https://raw.githubusercontent.com/zekgt/lua/main/phantom%20forces/libraries/ESP.lua"))()
+
+getgenv().ghosts = game.Workspace.Players['Bright orange']
+getgenv().phantoms = game.Workspace.Players['Bright blue']
+
+esp.Boxes = false
+esp.Names = false
+
+esp:AddObjectListener(ghosts, {
+  Recursive = true,
+  Type = "Model",
+  CustomName = " ",
+  Color = Color3.fromRGB(255, 165, 0),
+      Validator = function(obj)
+      return obj:FindFirstChild("Torso")
+  end,
+  IsEnabled = "ghosts"
+})
+
+esp:AddObjectListener(phantoms, {
+  Recursive = true,
+  Type = "Model",
+  CustomName = " ",
+  Color = Color3.fromRGB(0, 0, 233),
+      Validator = function(obj)
+      return obj:FindFirstChild("Torso")
+  end,
+  IsEnabled = "phantoms"
+})
+
+esp.ghosts = false
+esp.phantoms = false 
