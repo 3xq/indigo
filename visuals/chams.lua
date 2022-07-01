4af7fee0e43aaf3168300a2574e3a6a131108aa9
komt <64824301+3xq@users.noreply.github.com>
2022-07-01 09:36:43 -0500

    Update chams.lua

--- visuals/chams.lua
+++ visuals/chams.lua
@@ -1,1 +1,27 @@
+getgenv().chams = {}
 
+local function cham(model)
+    local highlight = Instance.new('Highlight', model)
+    highlight.Enabled = false
+  
+    highlight.FillColor = Color3.fromRGB(11, 78, 150)
+    highlight.FillTransparency = 0.3
+    
+    highlight.OutlineColor = Color3.fromRGB(200, 200, 200)
+    highlight.OutlineTransparency = 0.3
+    highlight.DepthMode = 0
+    
+    table.insert(highlights, highlight)
+
+    return highlight
+end
+
+-- initiate chams
+
+getgenv().update_chams = function()
+    for _, character in ipairs(characters) do
+        cham(character)
+    end
+end
+
+update_chams()
