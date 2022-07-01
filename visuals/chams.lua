c6336f3e5949a77c671da73c1e4733455ca639f6
komt <64824301+3xq@users.noreply.github.com>
2022-07-01 09:54:16 -0500

    Update chams.lua

--- visuals/chams.lua
+++ visuals/chams.lua
@@ -1,17 +1,25 @@
-getgenv().chams = {}
+getgenv().chams = {
+    ['enabled'] = false,
+    ['ghosts'] = {['enabled']=false,['objects']={}},
+    ['phantoms'] = {}
+}
 
-local function cham(model)
+local function cham(model, color)
     local highlight = Instance.new('Highlight', model)
     highlight.Enabled = false
   
-    highlight.FillColor = Color3.fromRGB(11, 78, 150)
+    highlight.FillColor = color
     highlight.FillTransparency = 0.3
     
     highlight.OutlineColor = Color3.fromRGB(200, 200, 200)
     highlight.OutlineTransparency = 0.3
     highlight.DepthMode = 0
     
-    table.insert(highlights, highlight)
+    if model.Parent.Name == 'Bright blue' then
+        table.insert(chams.phantoms.objects, model)
+    else
+        table.insert(chams.ghosts.objects, model)
+    end
 
     return highlight
 end
@@ -20,8 +28,20 @@ end
 
 getgenv().update_chams = function()
     for _, character in ipairs(characters) do
-        cham(character)
+        if character.Parent = 'Bright blue' then
+        cham(character, Color3.fromRGB(13, 105, 172))
+        else
+        cham(character, Color3.fromRGB(218, 133, 65))
+        end
     end
 end
+
+game:GetService("Workspace").Players["Bright blue"].ChildAdded:Connect(function(character)
+    cham(character, Color3.fromRGB(13, 105, 172))
+end)
+
+game:GetService("Workspace").Players["Bright orange"].ChildAdded:Connect(function(character)
+    cham(character, Color3.fromRGB(218, 133, 65))
+end)
 
 update_chams()
