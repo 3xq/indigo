a5bcac9540289b5d50d9c6a8c4427ab83b64d93b
komt <64824301+3xq@users.noreply.github.com>
2022-07-01 10:08:14 -0500

    Update debug-color.lua

--- indigo-cosmetics/debug-color.lua
+++ indigo-cosmetics/debug-color.lua
@@ -1,6 +1,6 @@
 local developer_console = game:GetService("CoreGui").DevConsoleMaster.DevConsoleWindow.DevConsoleUI.MainView.ClientLog
 
-for _, object in ipairs(developer_console:GetChidlren()) do
+for _, object in ipairs(developer_console:GetChildren()) do
 	if object:FindFirstChild('msg') then
 		object:WaitForChild('msg').TextColor3 = Color3.fromRGB(6, 57, 112)
 	end
